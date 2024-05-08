970077e4dc7ca8ffbd0c3735fc12e855c92f0f90
Aryan Saxena <aryanwork926@gmail.com>
2024-05-08 20:43:57 +0530

    new files and folders

---
+++ src/app/data.ts
@@ -0,0 +1,28 @@
+
+
+export const faqs = [
+  {
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    isAccordionOpen: false
+  },
+  {
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    isAccordionOpen: false
+  },
+  {
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    isAccordionOpen: false
+  },
+  {
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    isAccordionOpen: false
+  }
+];
---
+++ src/app/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import destktopbg from "@/assets/images/background-pattern-desktop.svg";
+import mobilebg from "@/assets/images/background-pattern-mobile.svg";
+import starIcon from "@/assets/images/icon-star.svg";
+import Accordion from "./components/Accordion";
+import { faqs } from "./data";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen relative p-4 pb-10 bg-purple-100">
+      <div className="w-full h-auto absolute top-0 left-0">
+      <Image
+        src={destktopbg}
+        alt="destktop-bg"
+        className="w-full h-full object-cover hidden md:block "
+      />
+      <Image
+        src={mobilebg}
+        alt="mobile-bg"
+        className="w-full h-full object-cover md:hidden "
+      />
+    </div>
+
+      <section className="relative bg-white transition-all mx-auto  max-w-xs sm:max-w-2xl rounded-lg flex flex-col gap-4 mt-40 sm:mt-24 p-5">
+        <h1 className="flex gap-3 items-center">
+          
+          <Image src={starIcon} alt="star-icon" className=" h-6  w-auto" />
+          <span className="font-bold text-4xl">FAQs</span>
+        </h1>
+        <div className="flex flex-col gap-4 divide-y">
+          {faqs.map((d, i) => (
+            <Accordion
+              answer={d.answer}
+              question={d.question}
+              isAccordionOpen={d.isAccordionOpen}
+              key={i}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
+
+
